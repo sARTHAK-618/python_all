d54ae01ea4dc8340bd3bf2b09285707f361738d1
sARTHAK-618 <sarthakshelar618@gmail.com>
2024-02-13 20:41:52 +0530

    why change

---
+++ day_1.py
@@ -0,0 +1,7 @@
+
+a = 1
+if a is int():
+    print("a is an integer")
+elif a <= int():
+    print("Akshata")
+print("end of code")
---
+++ the_hard_way/ex13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second, third= argv
+print("The script is called: ", script )
+print("You first variable is: ", first)
+print("You second variable is: ", second)
+print("You third varibale is: ", third)
