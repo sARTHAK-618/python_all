3bc53efc017156a45cf378dbee466027627abe76
sARTHAK-618 <sarthakshelar618@gmail.com>
2024-03-03 11:09:24 +0530

    Change_name____completed_project

---
+++ python_tkinter/tkinter_projects_tut/login_pjt_hala/main.py
@@ -0,0 +1,37 @@
+import tkinter
+from tkinter import messagebox
+
+window = tkinter.Tk()
+window.title("Login Form")
+window.geometry("300x400")
+window.configure(bg="#333333")
+
+def login():
+    username="johnsmith"
+    password="12345"    
+    if username_entry.get()==username and password_entry.get()==password:
+        messagebox.showinfo(title="Login Success", message="You successfully logged in")
+    else:
+        messagebox.showerror(title="Error", message="Invalid Login")
+
+frame = tkinter.Frame(bg="#333333")
+
+#creating Widgets
+login_label= tkinter.Label(frame, text="Login", bg="#333333", fg="#FF3399", font=("Arial", 30) )
+username_label = tkinter.Label(frame, text="Username", bg="#333333", fg="#FFF", font=("Arial", 16))
+username_entry = tkinter.Entry(frame, font=("Arial", 16))
+password_entry = tkinter.Entry(frame, show="*", font=("Arial", 16))
+password_label = tkinter.Label(frame, text="Password", bg="#333333", fg="#FFF", font=("Arial", 16))
+password_button = tkinter.Button(frame, text = "Login", bg="#FF3399", font=("Arial", 16), command=login)
+
+#Placing Widgets on the screen
+login_label.grid(row=0, column=0, columnspan=2, sticky="news", pady=40)
+username_label.grid(row=1, column=0)
+username_entry.grid(row=1, column=1, pady=20)
+password_label.grid(row=2, column=0)
+password_entry.grid(row=2, column=1, pady=20)
+password_button.grid(row=3, column=0, columnspan=2, pady=30)
+
+frame.pack()
+
+window.mainloop()
